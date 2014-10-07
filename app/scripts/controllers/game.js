804b0562c9e0f82d9cf532a75d40a78e687ffc91
Danny Eck <deck0@searshc.com>
2014-10-07 17:26:06 -0500

    *burp*

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -5,15 +5,7 @@ var GameCtrl = function ($scope, $interval) {
 
   // Init Game
   $scope.game = {};
-  $scope.game.board = new Board();
-  $scope.game.board.init({
-    rows: 9,
-    cols: 9,
-    bombs: 15
-  });
-
-  $scope.game.revealed = {};
-
+  $scope.game.board = new Board(9, 9, 15);
   window.game = $scope.game;
 
   var timer;
@@ -23,31 +15,36 @@ var GameCtrl = function ($scope, $interval) {
   }
 
   $scope.game.time = 0;
-  timer = $interval(
-    function(){
-      $scope.game.time++;
-      if ($scope.game.time > 999) {
-        stopTimer();
-      }
-    },
-    1000
-  );
-  $scope.$on('$destroy', stopTimer);
 
+  var startTimer = function () {
+    timer = $interval(
+      function(){
+        $scope.game.time++;
+        if ($scope.game.time > 999) {
+          stopTimer();
+        }
+      },
+      1000
+    );
+  };
+
+  $scope.$on('$destroy', stopTimer);
 
   $scope.game.flagsCount = 0;
 
+  this.reveal = function(x, y) {
+    if (!timer) {
+      startTimer();
+    }
+    $scope.game.board.reveal(x,y);
+  };
+
+  this.getState = function (x, y) {
+    return $scope.game.board.states[x][y];
+  };
+
   this.sayHi = function() {
     return 'hi';
-  };
-
-  this.reveal = function (x, y) {
-    $scope.game.revealed['_'+x+'_'+y] = true;
-    console.log($scope.game.revealed);
-  };
-
-  this.isRevealed = function (x, y) {
-    return $scope.game.revealed['_'+x+'_'+y] || false;
   };
 };
 
@@ -70,7 +67,7 @@ angular.module('speedSweeperApp')
   return {
     restrict: 'E',
     scope: {
-      count: '='
+      count: '@'
     },
     controller: function ($scope) {
       $scope.getDigit = function (position) {
@@ -95,42 +92,6 @@ angular.module('speedSweeperApp')
   };
 })
 
-// .directive('grid', function() {
-//   return {
-//     restrict: 'E',
-//     require: '^game',
-//     scope: {
-//       ngModel: '='
-//     },
-//     templateUrl: 'ss.grid',
-//     controller: function ($scope) {
-
-//     },
-//     link: function (scope, elem, attrs, ctrl) {
-//       ctrl.sayHi();
-//       console.log(ctrl.sayHi());
-//     }
-//   };
-// })
-
-.directive('foo', function () {
-  return {
-    restrict: 'E',
-    // link: function (scope, element, attrs) {
-    //   return {
-    //     pre: function (tElem, tAttrs, transcludeFn) {
-    //       var tmpl = angular.element('<div>');
-    //       tmpl.append('<h2>Hello World</h2>');
-    //       tElem.replaceWith(tmpl);
-    //     },
-    //     post: function (scope, iElem, iAttrs, ctrl) {
-    //       console.log(iElem);
-    //     }
-    //   };
-    // }
-  };
-})
-
 // What about without the row/col messiness?
 .directive('grid', function () {
   return {
@@ -139,27 +100,7 @@ angular.module('speedSweeperApp')
     scope: {
       ngModel: '='
     },
-    // templateUrl: 'ss.grid',
-    compile: function (tElem, attrs) {
-
-      // var M = attrs.ngModel.length;
-      // var N = M[0].length;
-
-      var rowElem = angular.element('<div>');
-      rowElem.addClass('row');
-
-      var cellElem = angular.element('<div>');
-      cellElem.addClass('cell');
-      cellElem.html('foo');
-
-      for (var i = 0; i < 10; i++) {
-        rowElem.append(cellElem.clone());
-      }
-
-      tElem.html(rowElem.html());
-
-      // tElem.html(rowElem);
-    },
+    templateUrl: 'ss.grid',
     link: function (scope, elem, attrs, ctrl) {
       ctrl.sayHi();
       console.log(ctrl.sayHi());
@@ -179,17 +120,23 @@ angular.module('speedSweeperApp')
     template: '' +
       '<div' +
       '  class="cell"' +
-      '  ng-class="{unknown: !isRevealed}"' +
+      '  ng-class="getState()"' +
       '>x</div>',
     link: function (scope, elem, attrs, gameCtrl) {
 
-      scope.isRevealed = false;
+      scope.getState = function() {
+        switch (gameCtrl.getState(scope.x, scope.y)) {
+          case 0:
+            return 'unknown';
+          default:
+            return 'foo';
+        }
+      };
 
       elem.bind('click', function(){
         console.log(scope.x, scope.y);
         scope.$apply(function(){
           gameCtrl.reveal(scope.x, scope.y);
-          scope.isRevealed = true;
         });
       });
       // ctrl.sayHi();
@@ -205,15 +152,15 @@ angular.module('speedSweeperApp')
       '<div class="game">' +
       '  <div class="header">' +
       '    {{ time }} ' +
-      '    <counter count="game.time"></counter>' +
+      '    <counter count="{{game.time}}"></counter>' +
       '    <smiley></smiley>' +
-      '    <counter count="game.flagsCount"></counter>' +
+      '    <counter count="{{game.flagsCount}}"></counter>' +
       '  </div>' +
       '  <grid' +
-      '    ng-model="game.board.state"' +
-      '    M="game.board.state.length"' +
+      '    ng-model="game.board.states"' +
       '  ></grid>' +
       '</div>',
+    replace: true,
     controller: 'GameCtrl'
   };
 })
@@ -237,50 +184,6 @@ angular.module('speedSweeperApp')
     // scope: {
     //   board: '='
     // },
-    controller: function ($scope, $element, $attrs, ctrl) {
-
-      $scope.bar = 'BAR';
-
-      // $scope.foo = {a:1};
-
-      // $scope.setBoard = function setBoardFn (board) {
-      //   $scope.board = board;
-      // };
-
-      // $scope.board = new Board();
-      // window.board = $scope.board;
-      // $scope.board.init({
-      //   rows: 9,
-      //   cols: 9,
-      //   bombs: 15
-      // });
-
-      // $scope.clicking = false;
-      // $scope.overGrid = false;
-      // $scope.isCaution = false;
-
-      // $scope.mousedown = function() {
-      //   $scope.clicking = true;
-      //   console.log('started clicking');
-      // };
-
-      // $scope.mouseenter = function () {
-      //   $scope.overGrid = true;
-      //   console.log('on the grid');
-      // };
-
-      // $scope.mouseleave = function() {
-      //   $scope.overGrid = false;
-      //   console.log('off the grid');
-      // };
-
-      // $scope.mouseup = function(e) {
-      //   angular.element(e.target).triggerHandler('foo');
-      //   $scope.clicking = false;
-      //   console.log('stopped clicking');
-      // };
-    },
-
     link: function (scope, element, attr, ctrl) {
 
       scope.board = ctrl.board;
@@ -300,76 +203,3 @@ angular.module('speedSweeperApp')
     }
   };
 });
-
-// .directive('revealable', function() {
-//   return {
-//     restrict: 'A',
-//     require: 'cell',
-//     // controller: function ($scope, $element, $attrs, )/
-//     link: function postLink (scope, iElem, iAttrs, ctrl) {
-//       // debugger;
-//     }
-//   };
-// })
-
-// .directive('cell', function() {
-//   return {
-//     restrict: 'A',
-//     // scope: {
-//     //   x: '=',
-//     //   y: '=',
-//     //   val: '=',
-//     //   board: '='
-//     // },
-//     controller: function ($scope) {
-
-//       $scope.isRevealed = function () {
-//         return $scope.board.revealed['_' + $scope.x + '_' + $scope.y];
-//       };
-
-//       $scope.reveal = function(x, y) {
-//         console.log('wtf!!!');
-//         window.alert(x,y);
-//       };
-
-//       // $scope.reveal = board.reveal(x, y);
-
-//       $scope.isUnknown = false;
-//       // console.log($scope.x, $scope.y);
-//     },
-//     link: function (scope, element) {
-
-//       element
-
-//       .addClass('cell')
-
-//       // .attr('draggable', true)
-
-//       // .on('foo', function() {
-//       //   scope.isUnknown = false;
-//       //   element.off('foo');
-//       // })
-
-//       // .bind('click', function (e) {
-//       //   console.log('clicked: ' + scope.x + ',' + scope.y);
-//       //   scope.$apply(function(){
-//       //     scope.board.reveal(scope.x, scope.y);
-//       //   });
-//       // })
-
-//       .bind('dragover', function (e) {
-//         e.preventDefault();
-//       })
-
-//       .bind('dragenter', function (e) {
-//         console.log('dragged into cell: ' + scope.x + ',' + scope.y);
-//         e.preventDefault();
-//       })
-
-//       .bind('drop', function (e) {
-//         console.log('Released on cell: ' + scope.x + ',' + scope.y);
-//         console.log(e);
-//       });
-//     }
-//   };
-// });
